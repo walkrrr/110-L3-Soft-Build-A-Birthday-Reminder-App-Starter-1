c422d3f907af970739fd9182a993d796fb03942e
RWR <62685147+walkrrr@users.noreply.github.com>
2021-06-30 04:11:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+user_input = input("Enter a person's name: ")
+
+if user_input == "Walkrrr":
+  print("Birthday is 12/26/1983!")
+elif user_input == "Missy": 
+    print("Birthday is 05/03/2006!")
+elif user_input == "DeVon":
+    print("Birthday is 06/23/1983")
+else:
+      print("Can't find a birthday for this person")
+
